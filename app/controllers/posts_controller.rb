74157956b44833b4629c550c11fc93328602b79c
Matt Wilkinson <mytematt@gmail.com>
2018-09-08 13:18:18 -0600

    third commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  # before_action :set_user, only: [:create]
   before_action :set_posts, only: [:show, :update, :edit, :destroy]
   
   def index
@@ -6,28 +7,51 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render partial: "form"
+    end
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      redirect :edit
+    end
   end
 
   def destroy
+    @posts.destroy
+    redirect_to posts_path
   end
 
   def show
   end
 
   def edit
+    render partial: "form"
   end
 
   def new
     @post = Post.new
+    render partial: "form"
   end
 
 private
 
+def set_user
+  @user = User.find(params[:user_id])
+end
+
 def set_posts
   @post = Post.find(params[:id])
+end
+
+def post_params
+  params.require(:post).permit(:content)
 end
 
 end
